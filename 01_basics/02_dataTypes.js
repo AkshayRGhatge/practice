14d1acab8d03e2722080f783ce66b41ffa6230fb
Akshay Ghatge <96392298+AkshayRGhatge@users.noreply.github.com>
2023-12-14 02:50:57 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as new version
+
+//alert("Hello")// we are using nodejs not browser
+
+console.log(3+3);
+console.log("Akshay");
+
+let name="akshay";
+let age=18;
+let isLoggedIn=false;
+
+//Data Type
+
+//Primitive datatypes
+// number
+// bigint
+// string
+// boolean
+// null => standalone value or empty
+// undefined => value does not assigned
+// symbol => unique
+
+//object
+
+
+console.log(typeof undefined) // undefined
+console.log(typeof null)// object
