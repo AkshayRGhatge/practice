cb7d0f433c78b6e6dd81f3859f049ecf248dcb7b
Akshay Ghatge <96392298+AkshayRGhatge@users.noreply.github.com>
2023-12-14 23:44:41 +0000

    Conversion of Data Types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score=null;
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//converting to number if the value is 
+/*
+
+"33"==>33,
+"33abc"--> NaN
+true==>1; false==>0
+""==>0
+null ==> 0
+*/
+
+let isLoggedIn =undefined
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+//1==>True; 0 ==>false
+//"" => false
+//"Akshay" => true
+// null or undefined ==> false
+
+let someNumber=undefined;
+
+let convertStringNumber=String(someNumber);
+console.log(convertStringNumber)
+console.log(typeof convertStringNumber)
