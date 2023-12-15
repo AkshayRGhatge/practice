88ff9beed346888f923467172e3c4d21b68c53fc
Akshay Ghatge <96392298+AkshayRGhatge@users.noreply.github.com>
2023-12-15 00:23:22 +0000

    conversion operator

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -30,5 +30,37 @@ let booleanIsLoggedIn=Boolean(isLoggedIn)
 let someNumber=undefined;
 
 let convertStringNumber=String(someNumber);
-console.log(convertStringNumber)
-console.log(typeof convertStringNumber)
+//console.log(convertStringNumber)
+//console.log(typeof convertStringNumber)
+
+
+
+//****************** Operations *********************/
+
+let value=3
+let negValue=- value
+//console.log(negValue)
+
+// console.log(2+2);
+// console.log(2*2);
+// console.log(2-2);
+// console.log(2/2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1="Hello";
+let str2="Akshay"
+let str3=str1 + " " + str2;
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log("1"+"2")
+// console.log(1+2+"3")
+// console.log("1"+2+2)
+
+//console.log(+true);
+//console.log(+"");
+
+let gameCounter =100
+++gameCounter;
+console.log(gameCounter);
