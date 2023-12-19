dd4d5d5cf380c1405216947b56a367deec176205
Akshay Ghatge <96392298+AkshayRGhatge@users.noreply.github.com>
2023-12-19 02:29:25 +0000

    num and maths

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,42 @@
+const score=400
+
+const balance =new Number(100) //use to strictly give number data type
+// console.log(balance);
+
+// console.log(balance.toString().length)
+
+// console.log(balance.toFixed(2));
+
+const otherNum=23.8966;
+// console.log(otherNum.toPrecision(3));
+
+
+const hundred=1000000
+// console.log(hundred.toLocaleString())
+
+//----------------Maths--------------------------------
+
+//Absolute value converts negative value to positive
+//Round round the figure
+//Ceil top value
+//floor value to pick down value
+//Max value pick maximum value
+//Min value pick minimum value
+
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.98));
+// console.log(Math.ceil(4.9));
+// console.log(Math.floor(4.98));
+// console.log(Math.max(4,3,9,1))
+// console.log(Math.min(4,3,9,1))
+
+//console.log(Math.random())//value comes between 0 and 1
+
+// console.log(Math.floor((Math.random()*10)+1));
+
+
+
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*10+1)+min)
